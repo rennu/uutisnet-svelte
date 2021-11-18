27768a3cb20adc7da419ab9ac7d01950a0d406e2
Sakari <renfld@gmail.com>
2021-11-18 09:16:47 +0200

    Fix dumb mistake

--- src/views/News.svelte
+++ src/views/News.svelte
@@ -1,21 +1,24 @@
 <script>
+  import { onMount } from 'svelte'
   import { showError } from '../actions'
   import NewsList from '../components/NewsList.svelte'
 
   let news = []
   let loading = true
 
-  fetch('/api/news')
-    .then(response => response.json())
-    .then(data => {
-      news = data.news
-      loading = false
-    })
-    .catch(err => {
-      loading = false
-      console.log(err)
-      showError('Takapäähän ei saatu yhteyttä :(')
-    })
+  onMount(async () => {
+    fetch('/api/news')
+      .then(response => response.json())
+      .then(data => {
+        news = data.news
+        loading = false
+      })
+      .catch(err => {
+        loading = false
+        console.log(err)
+        showError('Takapäähän ei saatu yhteyttä :(')
+      })
+  })
 </script>
 
 <NewsList {news} />
